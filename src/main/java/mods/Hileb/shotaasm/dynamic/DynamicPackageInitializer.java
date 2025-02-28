523ee72768ddc364b09a889541c89b79201b3ad6
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-02-28 23:30:12 +0800

    init

--- src/main/java/mods/Hileb/shotaasm/dynamic/DynamicPackageInitializer.java
+++ src/main/java/mods/Hileb/shotaasm/dynamic/DynamicPackageInitializer.java
@@ -4,10 +4,10 @@ import mods.Hileb.shotaasm.api.*;
 
 public class DynamicPackageInitializer {
     public static void initialize() {
-        IScriptCompiler.class;
-        IScriptLocator.class;
-        ScriptFile.class;
-        ShotaContext.class;
-        TransformerRegistry.class;
+        IScriptCompiler.class.getName();
+        IScriptLocator.class.getName();
+        ScriptFile.class.getName();
+        ShotaContext.class.getName();
+        TransformerRegistry.class.getName();
     }
 }
