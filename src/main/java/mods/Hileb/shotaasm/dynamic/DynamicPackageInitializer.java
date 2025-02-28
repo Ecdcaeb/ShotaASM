0fc40bbc1de9395a8690d3afea97dc8d2383be58
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-02-28 23:27:15 +0800

    implements the api packages

--- src/main/java/mods/Hileb/shotaasm/dynamic/DynamicPackageInitializer.java
+++ src/main/java/mods/Hileb/shotaasm/dynamic/DynamicPackageInitializer.java
@@ -1,5 +1,13 @@
 package mods.Hileb.shotaasm.dynamic;
 
+import mods.Hileb.shotaasm.api.*;
+
 public class DynamicPackageInitializer {
-    public static void initialize() {}
+    public static void initialize() {
+        IScriptCompiler.class;
+        IScriptLocator.class;
+        ScriptFile.class;
+        ShotaContext.class;
+        TransformerRegistry.class;
+    }
 }
